2deb3feb6736e7faf19c05d228f0954c0ecd12d3
dangrammer <danieljromans@gmail.com>
2020-02-06 18:30:48 -0500

    example completed

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
-//this file is optional
-//you can use this to paste in js code and have it run when index.html is loaded
+let element = document.createElement('div')
+let ul = document.createElement('ul')
+
+element.innerHTML = 'Hello, DOM!'
+element.style.backgroundColor = '#f9f9f9'
+element.style.textAlign = 'center'
+
+ul.style.textAlign = 'left'
+
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li')
+
+  li.innerHTML = (i + 1).toString()
+  ul.appendChild(li)
+}
+
+element.appendChild(ul)
+document.body.appendChild(element)
+
+// ul.removeChild(ul.querySelector('li:nth-child(2)'))
+// ul.remove()
